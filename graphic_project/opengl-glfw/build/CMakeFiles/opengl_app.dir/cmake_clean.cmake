cf7ec64333491edb46bb2419bb70f9d4c1eefcdf
Erik Tatsuya <erik.tatsuya@gmail.com>
2026-01-09 17:15:00 -0300

    primeira janela com glfw!

---
+++ graphic_project/opengl-glfw/build/CMakeFiles/opengl_app.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/opengl_app.dir/link.d"
+  "CMakeFiles/opengl_app.dir/main.cpp.o"
+  "CMakeFiles/opengl_app.dir/main.cpp.o.d"
+  "opengl_app"
+  "opengl_app.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/opengl_app.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
